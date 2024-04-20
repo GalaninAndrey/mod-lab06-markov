83e9baa9effd8bce8169b7f8d4f7b6aa300c8df7
Andrey Galanin <galaninan8@gmail.com>
2024-04-20 21:41:42 +0300

    fix commit

--- test/tests.cpp
+++ test/tests.cpp
@@ -40,6 +40,6 @@ TEST(test5, CreateText) {
         { { "wha" }, { "abody" } }
     };
     generator.set(str1);
-    std::string str = generator.Create(10, 1);
+    std::string str = generator.Create(10, 20);
     EXPECT_EQ("wha abody ", str);
 }
