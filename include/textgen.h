89e4b09f38e9535a7dad213c1e1f1e121bb5374a
Andrey Galanin <galaninan8@gmail.com>
2024-04-20 20:56:57 +0300

    Галанин Андрей Александрович Писм

--- include/textgen.h
+++ include/textgen.h
@@ -12,13 +12,13 @@ typedef std::deque<std::string> prefix;  // очередь префиксов
 
 class GenerationText {
  private:
-   std::map<prefix, std::vector<std::string>> statetab; // префикс-суффиксы
-   prefix fprefix;
+    std::map<prefix, std::vector<std::string>> statetab; // префикс-суффиксы
+    prefix fprefix;
  public:
-   void Learn(const std::string& string, int prefix_number);
-   std::string Create(int length, int seed, int size);
-   void set(std::map<prefix, std::vector<std::string>> string);
-   std::map<prefix, std::vector<std::string>> get();
+    void Learn(const std::string& string, int prefix_number);
+    std::string Create(int length, int seed, int size);
+    void set(std::map<prefix, std::vector<std::string>> string);
+    std::map<prefix, std::vector<std::string>> get();
 };
 
 #endif  // INCLUDE_TEXTGEN_H_
